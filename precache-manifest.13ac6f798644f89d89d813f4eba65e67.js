d46864439bd8c91c927a9f0df66b0fb2b1d20261
Matias Funes <matiasfunes94@gmail.com>
2020-11-08 18:42:45 -0300

    Updates

---
+++ precache-manifest.13ac6f798644f89d89d813f4eba65e67.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9adb0c0e5dd145a9f79b8bba4d1d118f",
+    "url": "/random-quotes-machine-hooks/index.html"
+  },
+  {
+    "revision": "e3459ac312bdd31fabb5",
+    "url": "/random-quotes-machine-hooks/static/css/2.fcd767de.chunk.css"
+  },
+  {
+    "revision": "0cce91e0b7002da95de0",
+    "url": "/random-quotes-machine-hooks/static/css/main.87e65fa6.chunk.css"
+  },
+  {
+    "revision": "e3459ac312bdd31fabb5",
+    "url": "/random-quotes-machine-hooks/static/js/2.794b1caa.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/random-quotes-machine-hooks/static/js/2.794b1caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cce91e0b7002da95de0",
+    "url": "/random-quotes-machine-hooks/static/js/main.896a3e7b.chunk.js"
+  },
+  {
+    "revision": "8fc4abedd16c32e907b4",
+    "url": "/random-quotes-machine-hooks/static/js/runtime-main.fa3adb72.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/random-quotes-machine-hooks/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/random-quotes-machine-hooks/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/random-quotes-machine-hooks/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/random-quotes-machine-hooks/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/random-quotes-machine-hooks/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
